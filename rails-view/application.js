140d970de79d11688faca752bbac1ff837c82632
Phil LaPier <plapier@thoughtbot.com>
2011-02-15 14:52:07 -0500

    Added alternative animation - slide down then slide up after 4 second delay

--- rails-view/application.js
+++ rails-view/application.js
@@ -1,8 +1,11 @@
 $(function(){
 
     $('.flash_message')
-      .animate({ marginTop: 0 }
-    );
+      .animate({ marginTop: 0 }); // Slide flash message down
+
+  // Alternatively, Slide down then Slide up after delay of 4 seconds
+  //  .animate({ marginTop: 0 }).delay(4000).animate({ marginTop: -50 });
+
     $('#flash_close').click(function(){
       $('.flash_message').animate({ marginTop: -50 })
     });
