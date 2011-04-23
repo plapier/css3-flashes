be7b79cb00b440ee8cc20f8d76b4c260c0b5bcc0
Phil LaPier <plapier@thoughtbot.com>
2011-04-23 19:05:04 -0400

    Added new light-chrome version

---
+++ light-chrome/js/application.js
@@ -0,0 +1,17 @@
+//----------------------------------------------------------------
+// This Javascript is required for Flashes to animate in your app.
+//----------------------------------------------------------------
+
+$(function(){
+
+    $('.flash_message')
+      .animate({ marginTop: 0 }); // Slide flash message down
+
+  // Alternatively, Slide down then Slide up after delay of 4 seconds
+  //  .animate({ marginTop: 0 }).delay(4000).animate({ marginTop: -55 });
+
+    $('#flash_close').click(function(){
+      $('.flash_message').animate({ marginTop: -55 })
+    });
+
+});
---
+++ light-chrome/js/flashes.js
@@ -0,0 +1,46 @@
+//---------------------------------------------------
+// Javascript for Demo Page only. See application.js.
+//---------------------------------------------------
+
+var slideDown = function(pos){
+  $('.flash_message').animate({ marginTop: 0 });
+};
+var slideUp = function(btnID){
+  $('.flash_message').animate({ marginTop: -55 }, function(){ changeIcon(btnID) });
+};
+
+var changeIcon = function(btnID){
+
+  var iconID = "icon_" + btnID;
+    $('div#icon_status').attr("class", iconID);
+
+  if (btnID == "notice") {
+    $('div.flash_message p').html("Woah! Slow down, I can't think that fast.");
+  }
+  else if (btnID == "failure") {
+    $('div.flash_message p').html("Incorrect email or password. Please try again.");
+  }
+  else if (btnID == "success") {
+    $('div.flash_message p').html("Account created successfully!");
+  }
+  else {
+    return false;
+  }
+};
+
+$(function(){
+  // Slide down on page load
+  slideDown();
+
+  // On button click, slide up - change icon and text - slide down
+  $('li button').click(function(){
+    var btnID = $(this).attr("id");
+      slideUp(btnID);
+      slideDown(btnID);
+  });
+
+  // Slide up when close button is clicked
+  $('#flash_close').click(function(){
+    slideUp();
+  });
+});
