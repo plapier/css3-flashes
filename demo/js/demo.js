4d109d5ff03c3527787b6b929c187551c9334a71
Adam Anderson <adamandersonis@gmail.com>
2011-07-22 07:58:14 -0700

    Update demo to use jQuery plugin

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -1,52 +1,60 @@
-//---------------------------------------------------
-// Javascript for Demo Page only. See application.js.
-//---------------------------------------------------
+(function($) {
+  $.fn.sexyFlash = function(settings) {
+    settings = $.extend({
+      delayTime: 3000,
+      boxShadowOffset: 2
+    }, settings);
 
-var slideDown = function(pos){
-  $('.flash_message').animate({ marginTop: 0 });
-};
-var slideUp = function(closePos, btnID){
-  $('.flash_message').animate({ marginTop: closePos }, function(){ changeIcon(btnID) });
-};
+    function stopAnimation() {
+      $(this).clearQueue();
+    }
+    function restartAnimation() {
+      $(this).delay(settings.delayTime).slideUp();
+    }
+    function closeFlash() {
+      $(this).clearQueue();
+      $(this).parents('.flash_message').slideUp();
+    }
 
-var changeIcon = function(btnID){
+    return this.each(function() {
+      var flashHeight = $(this).innerHeight();
+      var closePos = "-" + (parseInt(flashHeight) + settings.boxShadowOffset) + "px";
 
-  var iconID = "icon_" + btnID;
-    $('div#icon_status').attr("class", iconID);
+      $('.flash_close', this).click(closeFlash);
+      $(this)
+        .hover(stopAnimation, restartAnimation)
+        .animate({ marginTop: 0 })
+        .delay(settings.delayTime)
+        .slideUp();
+    });
+  };
+})(jQuery);
 
-  if (btnID == "notice") {
-    $('div.flash_message p').html("Woah! Slow down, I can't think that fast.");
-  }
-  else if (btnID == "failure") {
-    $('div.flash_message p').html("Incorrect email or password. Please try again.");
-  }
-  else if (btnID == "success") {
-    $('div.flash_message p').html("Account created successfully!");
-  }
-  else {
-    return false;
-  }
-};
+(function($) {
+  $(function() {
+    var messages = {
+      failure: "Incorrect email or password. Please try again.",
+      notice: "Woah! Slow down, I can't think that fast.",
+      success: "Account created successfully!"
+    };
 
-$(function(){
-  // Slide down on page load
-  slideDown();
-  var flashHeight = $('div#flash').css("max-height");             // Grab height of Flash Message
-  var boxShadowOffset = 2;                                        // Define CSS Box-Shadow Height (pixels)
+    var flashMessageHTML = '<div class="flash_message"> \
+      <div class="flash_icon"> \
+        <div class="icon_status"> \
+          <div class="gloss"></div> \
+        </div> \
+      </div> \
+      <div class="flash-vrule"></div> \
+      <p></p> \
+      <div class="flash_close"><span class="close">&#215;</span></div> \
+    </div>';
 
-  // Calculate closed position of flash message
-  // Negate height, parse to integer, add CSS shadow height
-  var closePos = "-" + (parseInt(flashHeight) + boxShadowOffset) + "px";
-
-  // On button click, slide up - change icon and text - slide down
-  $('li button').click(function(){
-    var btnID = $(this).attr("id");
-      slideUp(closePos, btnID); // pass in flash message height & button ID
-      slideDown(btnID);
+    $('#buttons button').click(function() {
+      var flash = $(flashMessageHTML);
+      var buttonID = $(this).attr('id');
+      flash.find('p').text(messages[buttonID]);
+      flash.find('.icon_status').addClass('icon_' + buttonID);
+      $('#flash').append(flash).find('.flash_message').sexyFlash();
+    });
   });
-
-  // Slide up when close button is clicked
-  $('#flash_close').click(function(){
-    slideUp(closePos);
-  });
-});
+})(jQuery);
